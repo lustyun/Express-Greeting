b27e071013fd00b30b84eac53d7b652e5058240e
Fardus Ahmed <fardus11@gmail.com>
2023-08-16 15:43:33 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const plants = [
+    "Monstera Deliciosa",
+    "Corpse Flower",
+    "Elephant-Foot Yam",
+    "Witches' Butter",
+];
+
+app.get("/greeting", (req, res) => {
+    //this will never be reached
+    res.send("<h1>Hello, stranger!</h1>");
+});
+
+app.get("/greeting/:name", (req, res) => {
+    //this will never be reached
+    res.send(`<h1>Hello there, ${req.params.name}!</h1>`);
+});
+
+app.listen(port, () => {
+    console.log("listening on http://localhost:3000/ port", port);
+});
